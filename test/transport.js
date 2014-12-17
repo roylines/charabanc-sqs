e6a848ed63d929444d783153e3775e0475518672
Roy Lines <roy@roylines.co.uk>
2014-12-17 20:15:12 +0000

    initial bootstrap test

--- test/transport.js
+++ test/transport.js
@@ -1,5 +1,36 @@
-var t = require('../lib/transport');
+var charabanc = require('charabanc'),
+  chai = require('chai'),
+  sinon = require('sinon'),
+  t = require('../lib/transport');
+
+chai.should();
 
 describe('transport', function() {
-  it('should...', function() {});
+  before(function() {
+    charabanc.setTransport(t);
+  });
+
+  it('can call configure', function(done) {
+    return charabanc.configure({}, done);
+  });
+
+  describe('with one service', function() {
+    var service;
+    before(function(done) {
+      service = sinon.stub();
+      service.yields(null, { response: 'RESPONSE' });
+      return charabanc.register('act', service, done);
+    });
+    after(function(done) {
+      return charabanc.reset(done);
+    });
+    it('should call service', function(done) {
+      charabanc.request('act', {
+        payload: 'PAYLOAD'
+      }, function(e, response) {
+        response.should.deep.equal({ response: 'RESPONSE' });
+        return done(e);
+      });
+    });
+  });
 });
